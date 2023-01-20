47b6e5e5b05f2e92337cfab05c0f6d580f844c54
Simone Marino <simo.marino@outlook.it>
2023-01-20 16:51:53 +0100

    Password generator

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// PROMPT ASSEGNATO ALLA VARIABILE nome
+let nome = prompt('Qual\' é il tuo nome?')
+// console.log(nome)
+
+// PROMPT ASSEGNATO ALLA VARIABILE cognome
+let cognome = prompt('Qual\' é il tuo cognome?')
+// console.log(cognome)
+
+// PROMPT ASSEGNATO ALLA VARIABILE colore
+let colore = prompt('Qual\' é il tuo colore preferito?')
+// console.log(colore)
+
+// VARIABILE NUMERICA number
+let number = 23
+// console.log(number)
+
+// STAMPA PSW IN PAGINA
+document.getElementById('password_gen').innerHTML = nome + cognome + colore + number
+
